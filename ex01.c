9f4f0b9fd7863cca4b05d5f4fb05dd411e21da35
gianandreadigennaro <162984686+gianandreadigennaro@users.noreply.github.com>
2024-03-11 15:59:03 +0000

    ?

---
+++ ex01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+int a;
+int b;
+int c;
+int d;
+printf("inserisci un numero \n");
+scanf("%d %d %d %d", &a, &b, &c, &d);
+printf("numeri inseriti %d %d %d %d\n", a+b+c+d);
+}
---
+++ ex02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+int a;
+int b;
+int c;
+int d;
+printf("inserisci un numero \n");
+scanf("%d %d %d %d", &a, &b, &c, &d);
+printf("numeri inseriti %d\n", a+b+c+d);
+
+}
